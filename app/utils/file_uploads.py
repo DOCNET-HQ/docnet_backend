5de72919cb33e1e091ae4f892de9743968f8279e
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2025-09-04 16:28:17 +0200

    Added utility functions for file uploads.

---
+++ app/utils/file_uploads.py
@@ -0,0 +1,28 @@
+import uuid
+import os
+
+
+def upload_id_documents(instance, filename):
+    """Upload function for ID documents"""
+    ext = filename.split('.')[-1]
+    unique_filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join("users/profile/id_documents", unique_filename)
+
+
+def upload_profile_photo(instance, filename):
+    """Upload function for ID documents"""
+    ext = filename.split('.')[-1]
+    unique_filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join("users/profile/photos", unique_filename)
+
+def upload_doctors_license(instance, filename):
+    """Upload function for Doctor's license documents"""
+    ext = filename.split('.')[-1]
+    unique_filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join("users/profiles/doctors/licenses", unique_filename)
+
+def upload_hospitals_license(instance, filename):
+    """Upload function for Hospital's license documents"""
+    ext = filename.split('.')[-1]
+    unique_filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join("users/profiles/hospitals/licenses", unique_filename)
