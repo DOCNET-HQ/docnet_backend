e7fce7291fef196eee4faa0c1391f74b19adc584
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2025-09-07 12:34:30 +0200

    Modified the app admin

--- app/admins/admin.py
+++ app/admins/admin.py
@@ -1,3 +1,36 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from .models import AdminProfile
+
+@admin.register(AdminProfile)
+class AdminProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'user', 'name', 'phone_number'
+    ]
+    search_fields = [
+        'user__email', 'user__username', 'name', 'phone_number'
+    ]
+    readonly_fields = ['id']
+    ordering = ('name',)
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'user',
+                    'name',
+                    'phone_number',
+                    'photo',
+                )
+            }
+        ),
+        (
+            _('System Information'),
+            {
+                'fields': (
+                    'id',
+                )
+            }
+        ),
+    )
