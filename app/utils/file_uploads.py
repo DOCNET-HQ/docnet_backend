d4c3d10e9ef318e40e723d78b47d69cb8af51f8a
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2025-09-08 13:46:54 +0200

    Modified the file uploads util functions

--- app/utils/file_uploads.py
+++ app/utils/file_uploads.py
@@ -15,11 +15,13 @@ def upload_profile_photo(instance, filename):
     unique_filename = f"{uuid.uuid4()}.{ext}"
     return os.path.join("users/profile/photos", unique_filename)
 
+
 def upload_doctors_license(instance, filename):
     """Upload function for Doctor's license documents"""
     ext = filename.split('.')[-1]
     unique_filename = f"{uuid.uuid4()}.{ext}"
     return os.path.join("users/profiles/doctors/licenses", unique_filename)
+
 
 def upload_hospitals_license(instance, filename):
     """Upload function for Hospital's license documents"""
