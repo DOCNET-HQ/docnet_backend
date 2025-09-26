1e6f05d31bf737a8fd27a781b0c7bdf86cdea837
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2025-09-26 07:24:26 +0200

    Modified the file upload utility functions

--- app/utils/file_uploads.py
+++ app/utils/file_uploads.py
@@ -6,25 +6,37 @@ def upload_id_documents(instance, filename):
     """Upload function for ID documents"""
     ext = filename.split('.')[-1]
     unique_filename = f"{uuid.uuid4()}.{ext}"
-    return os.path.join("users/profile/id_documents", unique_filename)
+    return os.path.join(
+        "media/users/profile/id_documents",
+        unique_filename
+    )
 
 
 def upload_profile_photo(instance, filename):
     """Upload function for ID documents"""
     ext = filename.split('.')[-1]
     unique_filename = f"{uuid.uuid4()}.{ext}"
-    return os.path.join("users/profile/photos", unique_filename)
+    return os.path.join(
+        "media/users/profiles/photos",
+        unique_filename
+    )
 
 
 def upload_doctors_license(instance, filename):
     """Upload function for Doctor's license documents"""
     ext = filename.split('.')[-1]
     unique_filename = f"{uuid.uuid4()}.{ext}"
-    return os.path.join("users/profiles/doctors/licenses", unique_filename)
+    return os.path.join(
+        "media/users/profiles/doctors/licenses",
+        unique_filename
+    )
 
 
 def upload_hospitals_license(instance, filename):
     """Upload function for Hospital's license documents"""
     ext = filename.split('.')[-1]
     unique_filename = f"{uuid.uuid4()}.{ext}"
-    return os.path.join("users/profiles/hospitals/licenses", unique_filename)
+    return os.path.join(
+        "media/users/profiles/hospitals/licenses",
+        unique_filename
+    )
