01d7258507af32ee2e395d87a4df5c8ae59813f4
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-18 23:35:24 +0200

    Modified

--- app/admins/admin.py
+++ app/admins/admin.py
@@ -6,41 +6,35 @@ from .models import AdminProfile
 @admin.register(AdminProfile)
 class AdminProfileAdmin(admin.ModelAdmin):
     list_display = [
-        'user', 'name', 'phone_number', 'country',
+        "user",
+        "name",
+        "phone_number",
+        "country",
     ]
-    search_fields = [
-        'user__email', 'user__username', 'name', 'phone_number'
-    ]
-    readonly_fields = ['id']
-    ordering = ('name', 'country')
+    search_fields = ["user__email", "name", "phone_number"]
+    readonly_fields = ["id"]
+    ordering = ("name", "country")
     fieldsets = (
         (
             None,
             {
-                'fields': (
-                    'user',
-                    'name',
-                    'phone_number',
-                    'photo',
+                "fields": (
+                    "user",
+                    "name",
+                    "phone_number",
+                    "photo",
                 )
-            }
+            },
         ),
+        (_("System Information"), {"fields": ("id",)}),
         (
-            _('System Information'),
+            _("Location Information"),
             {
-                'fields': (
-                    'id',
+                "fields": (
+                    "country",
+                    "state",
+                    "city",
                 )
-            }
-        ),
-        (
-            _('Location Information'),
-            {
-                'fields': (
-                    'country',
-                    'state',
-                    'city',
-                )
-            }
+            },
         ),
     )
