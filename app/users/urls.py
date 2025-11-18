1e16f2acd0c11a3288d7c9a01ac1cd5e3abbfcef
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-18 23:35:29 +0200

    Modified

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,7 @@
 """
 URL mapping for users App APIs.
 """
+
 from django.urls import path
 from users import views
 from rest_framework_simplejwt.views import (
@@ -10,53 +11,28 @@ from rest_framework_simplejwt.views import (
 app_name = "users"
 
 urlpatterns = [
-    path(
-        "",
-        views.ListCreateUserView.as_view(),
-        name="users_list_create"
-    ),
-    path(
-        "<uuid:id>/",
-        views.DetailUserView.as_view(),
-        name="user_details"
-    ),
-    path(
-        "me/",
-        views.ManageProfileView.as_view(),
-        name="manage_profile"
-    ),
+    path("", views.ListCreateUserView.as_view(), name="users_list_create"),
+    path("<uuid:id>/", views.DetailUserView.as_view(), name="user_details"),
+    path("me/", views.ManageProfileView.as_view(), name="manage_profile"),
     path(
         "activate/<str:uidb64>/<str:token>/",
         views.ActivateAccountAPIView.as_view(),
-        name="activate",
+        name="activate_account",
     ),
+    path("password-reset/", views.PasswordResetView.as_view(), name="password_reset"),
     path(
-        'password-reset/',
-        views.PasswordResetView.as_view(),
-        name='password_reset'
-    ),
-    path(
-        'password-reset-confirm/<str:uidb64>/<str:token>/',
+        "password-reset-confirm/<str:uidb64>/<str:token>/",
         views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm'
+        name="password_reset_confirm",
     ),
     path(
-        "token/", views.JWTCookieTokenObtainPairView.as_view(),
-        name="token_obtain_pair"
+        "token/", views.JWTCookieTokenObtainPairView.as_view(), name="token_obtain_pair"
     ),
     path(
         "token/refresh/",
         views.JWTCookieTokenRefreshView.as_view(),
         name="token_refresh",
     ),
-    path(
-        "token/verify/",
-        TokenVerifyView.as_view(),
-        name="token_verify"
-    ),
-    path(
-        "logout/",
-        views.LogOutAPIView.as_view(),
-        name="token_verify"
-    ),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("logout/", views.LogOutAPIView.as_view(), name="token_verify"),
 ]
