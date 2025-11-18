c37d048b55e6041441fc3e57101123bde18aea94
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-18 23:35:26 +0200

    Modified

--- app/doctors/urls.py
+++ app/doctors/urls.py
@@ -1,77 +1,44 @@
 from django.urls import path
 from . import views
 
-app_name = 'doctors'
+app_name = "doctors"
 
 urlpatterns = [
+    path("", views.DoctorListView.as_view(), name="doctor-list"),
+    path("my-doctors/", views.MyDoctorListView.as_view(), name="my-doctor-list"),
+    path("create/", views.DoctorCreateView.as_view(), name="doctor-create"),
+    path("<uuid:id>/", views.DoctorDetailView.as_view(), name="doctor-detail"),
+    path("<uuid:id>/update/", views.DoctorUpdateView.as_view(), name="doctor-update"),
+    path("<uuid:id>/delete/", views.DoctorDeleteView.as_view(), name="doctor-delete"),
+    path("my-profile/", views.MyDoctorProfileView.as_view(), name="my-doctor-profile"),
     path(
-        '',
-        views.DoctorListView.as_view(),
-        name='doctor-list'
-    ),
-    path(
-        'create/',
-        views.DoctorCreateView.as_view(),
-        name='doctor-create'
-    ),
-    path(
-        '<uuid:id>/',
-        views.DoctorDetailView.as_view(),
-        name='doctor-detail'
-    ),
-    path(
-        '<uuid:id>/update/',
-        views.DoctorUpdateView.as_view(),
-        name='doctor-update'
-    ),
-    path(
-        '<uuid:id>/delete/',
-        views.DoctorDeleteView.as_view(),
-        name='doctor-delete'
-    ),
-
-    path(
-        'my-profile/',
-        views.MyDoctorProfileView.as_view(),
-        name='my-doctor-profile'
-    ),
-
-    path(
-        'my-basic-profile/',
+        "my-basic-profile/",
         views.MyBasicDoctorProfileView.as_view(),
-        name='doctor-basic-info'
+        name="doctor-basic-info",
     ),
-
     # doctor KYC Record CRUD URLs
     path(
-        'kyc-records/',
-        views.DoctorKYCRecordListView.as_view(),
-        name='doctor-kyc-list'
+        "kyc-records/", views.DoctorKYCRecordListView.as_view(), name="doctor-kyc-list"
     ),
     path(
-        'kyc-records/create/',
+        "kyc-records/create/",
         views.DoctorKYCRecordCreateView.as_view(),
-        name='doctor-kyc-create'
+        name="doctor-kyc-create",
     ),
     path(
-        'kyc-records/<int:id>/',
+        "kyc-records/<int:id>/",
         views.DoctorKYCRecordDetailView.as_view(),
-        name='doctor-kyc-detail'
+        name="doctor-kyc-detail",
     ),
     path(
-        'kyc-records/<int:id>/update/',
+        "kyc-records/<int:id>/update/",
         views.DoctorKYCRecordUpdateView.as_view(),
-        name='doctor-kyc-update'
+        name="doctor-kyc-update",
     ),
     path(
-        'kyc-records/<int:id>/delete/',
+        "kyc-records/<int:id>/delete/",
         views.DoctorKYCRecordDeleteView.as_view(),
-        name='doctor-kyc-delete'
+        name="doctor-kyc-delete",
     ),
-
-    path(
-        'stats/',
-        views.Doctor_stats,
-        name='doctor-stats'
-    ),
+    path("stats/", views.Doctor_stats, name="doctor-stats"),
 ]
