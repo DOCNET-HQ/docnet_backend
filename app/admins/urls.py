c9d2948040ab804086dc728bb31d8ace99bf82f8
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-18 23:35:24 +0200

    Modified

--- app/admins/urls.py
+++ app/admins/urls.py
@@ -1,37 +1,17 @@
 from django.urls import path
 from . import views
 
-app_name = 'admins'
+app_name = "admins"
 
 urlpatterns = [
+    path("", views.AdminListView.as_view(), name="admin-list"),
+    path("<uuid:id>/", views.AdminDetailView.as_view(), name="admin-detail"),
+    path("<uuid:id>/update/", views.AdminUpdateView.as_view(), name="admin-update"),
+    path("<uuid:id>/delete/", views.AdminDeleteView.as_view(), name="admin-delete"),
+    path("my-profile/", views.MyAdminProfileView.as_view(), name="my-admin-profile"),
     path(
-        '',
-        views.AdminListView.as_view(),
-        name='admin-list'
-    ),
-    path(
-        '<uuid:id>/',
-        views.AdminDetailView.as_view(),
-        name='admin-detail'
-    ),
-    path(
-        '<uuid:id>/update/',
-        views.AdminUpdateView.as_view(),
-        name='admin-update'
-    ),
-    path(
-        '<uuid:id>/delete/',
-        views.AdminDeleteView.as_view(),
-        name='admin-delete'
-    ),
-    path(
-        'my-profile/',
-        views.MyAdminProfileView.as_view(),
-        name='my-admin-profile'
-    ),
-    path(
-        'my-basic-profile/',
+        "my-basic-profile/",
         views.MyBasicAdminProfileView.as_view(),
-        name='admin-basic-info'
-    )
+        name="admin-basic-info",
+    ),
 ]
