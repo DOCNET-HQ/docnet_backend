55e92ab14573e4fc072452e7f429d6adca5068b9
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-18 23:35:28 +0200

    Modified

--- app/users/models.py
+++ app/users/models.py
@@ -34,7 +34,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
-        user.role = 'admin'
+        user.role = "admin"
         user.is_active = True
         user.is_staff = True
         user.is_superuser = True
@@ -48,16 +48,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     Custom user model.
     """
 
-    id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
-    email = models.EmailField(
-        max_length=255, unique=True, validators=[validate_email]
-    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(max_length=255, unique=True, validators=[validate_email])
 
-    role = models.CharField(
-        max_length=50, choices=USER_ROLES, default='patient'
-    )
+    role = models.CharField(max_length=50, choices=USER_ROLES, default="patient")
 
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
