fbf208aec52e21c0e2d4b72c81645556b000ffbc
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-18 23:35:29 +0200

    Modified

--- app/utils/permissions.py
+++ app/utils/permissions.py
@@ -9,9 +9,7 @@ class IsAdminOrCreateOnly(BasePermission):
 
 
 class IsOwnerOrAdmin(BasePermission):
-    message = (
-        "You must be the owner of this instance or an admin to perform this action."  # noqa
-    )
+    message = "You must be the owner of this instance or an admin to perform this action."  # noqa
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user or request.user.is_staff
@@ -33,25 +31,31 @@ class IsAdminOrReadOnly(BasePermission):
     message = "You must be an admin to modify this content. Read-only access is allowed for everyone."  # noqa
 
     def has_permission(self, request, view):
-        return request.method in ["GET", "HEAD", "OPTIONS"] or request.user.is_staff  # noqa
+        return request.method in ["GET", "HEAD", "OPTIONS"] or request.user.is_staff
 
 
 class IsHospital(BasePermission):
     message = "You must be associated with a hospital to perform this action."
 
     def has_permission(self, request, view):
-        return request.user.role == "hospital" and hasattr(request.user, 'hospital_profile') # noqa
+        return request.user.role == "hospital" and hasattr(
+            request.user, "hospital_profile"
+        )
 
 
 class IsDoctor(BasePermission):
     message = "You must be associated with a doctor to perform this action."
 
     def has_permission(self, request, view):
-        return request.user.role == "doctor" and hasattr(request.user, 'doctor_profile') # noqa
+        return request.user.role == "doctor" and hasattr(
+            request.user, "doctor_profile"
+        )  # noqa
 
 
 class IsPatient(BasePermission):
     message = "You must be associated with a patient to perform this action."
 
     def has_permission(self, request, view):
-        return request.user.role == "patient" and hasattr(request.user, 'patient_profile') # noqa
+        return request.user.role == "patient" and hasattr(
+            request.user, "patient_profile"
+        )  # noqa
