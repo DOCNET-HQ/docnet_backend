b25908205f207dc35a52e4672ea5c43573cbd3f6
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-18 23:35:26 +0200

    Modified

--- app/hospitals/models.py
+++ app/hospitals/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from utils.file_uploads import (
-    upload_hospitals_license
-)
 from utils.validations import validate_id_file
-from profiles.models import Profile, KYCRecord
+from profiles.models import Profile, KYCRecord, Specialty
 from django.contrib.auth import get_user_model
+from utils.file_uploads import upload_hospitals_license
+from utils.file_uploads import upload_cover_image
 
 
 User = get_user_model()
@@ -12,15 +11,22 @@ User = get_user_model()
 
 class Hospital(Profile):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE,
-        related_name='hospital_profile'
+        User, on_delete=models.CASCADE, related_name="hospital_profile"
     )
+
+    specialties = models.ManyToManyField(
+        Specialty,
+        related_name="hospitals",
+        blank=True,
+    )
+
+    cover_image = models.ImageField(upload_to=upload_cover_image, blank=True, null=True)
 
     registration_number = models.CharField(
         max_length=100,
         blank=True,
         null=True,
-        help_text='Official registration number of the hospital'
+        help_text="Official registration number of the hospital",
     )
 
     # License information
@@ -45,42 +51,41 @@ class Hospital(Profile):
         upload_to=upload_hospitals_license,
         blank=True,
         null=True,
-        validators=[validate_id_file]
+        validators=[validate_id_file],
     )
 
     def save(self, *args, **kwargs):
         if not self._state.adding:
             original = Hospital.objects.get(pk=self.pk)
             if (
-                original.license_number != self.license_number or
-                original.license_expiry_date != self.license_expiry_date or
-                original.license_document != self.license_document
+                original.license_number != self.license_number
+                or original.license_expiry_date != self.license_expiry_date
+                or original.license_document != self.license_document
             ):
                 # TODO: Send email to admin to verify
-                # TODO: Before they update the license details on the frontend, give them a warning saying message # noqa
+                # TODO: Before they update the license details on the frontend,
+                # give them a warning saying ---
                 # TODO: Send email to the hospital to notify them of the change
                 pass
 
         super().save(*args, **kwargs)
 
     class Meta:
-        verbose_name = 'Hospital'
-        verbose_name_plural = 'Hospitals'
-        ordering = ['-created_at']
+        verbose_name = "Hospital"
+        verbose_name_plural = "Hospitals"
+        ordering = ["-created_at"]
 
 
 class HospitalKYCRecord(KYCRecord):
     hospital = models.ForeignKey(
-        Hospital,
-        on_delete=models.CASCADE,
-        related_name='kyc_records'
+        Hospital, on_delete=models.CASCADE, related_name="kyc_records"
     )
     reviewed_by = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='hospital_kyc_reviews'
+        related_name="hospital_kyc_reviews",
     )
 
     def __str__(self):
@@ -89,4 +94,4 @@ class HospitalKYCRecord(KYCRecord):
     class Meta:
         verbose_name = "Hospital KYC Record"
         verbose_name_plural = "Hospital KYC Records"
-        ordering = ['-reviewed_at']
+        ordering = ["-reviewed_at"]
