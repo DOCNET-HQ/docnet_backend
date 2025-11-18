7eb2b9bac839234b2951547cb1bf1f84ce20db92
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-18 23:35:29 +0200

    Modified

--- app/utils/validations.py
+++ app/utils/validations.py
@@ -2,9 +2,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_id_file(value):
-    valid_extensions = ['pdf', 'jpg', 'jpeg', 'png']
-    ext = value.name.split('.')[-1].lower()
+    valid_extensions = ["pdf", "jpg", "jpeg", "png"]
+    ext = value.name.split(".")[-1].lower()
     if ext not in valid_extensions:
-        raise ValidationError(
-            'Only PDF, JPG, JPEG, and PNG files are allowed.'
-        )
+        raise ValidationError("Only PDF, JPG, JPEG, and PNG files are allowed.")
