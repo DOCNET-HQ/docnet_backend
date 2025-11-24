db3f017aef30f84bed28136c7a402a05c5d1a51a
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-24 08:40:13 +0200

    Modified

--- app/appointments/urls.py
+++ app/appointments/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AppointmentViewSet
+from .views import AppointmentViewSet, AppointmentStatsViewSet
 
 app_name = "appointments"
 
 router = DefaultRouter()
 router.register(r"appointments", AppointmentViewSet, basename="appointment")
+router.register(
+    r"appointment-stats", AppointmentStatsViewSet, basename="appointments-stats"
+)
 
 urlpatterns = [
     path("", include(router.urls)),
