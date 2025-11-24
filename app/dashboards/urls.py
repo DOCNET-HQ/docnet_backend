50771189e0169783ab143ec12bf9d0f81d44ba89
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2025-11-24 13:32:05 +0200

    Modified

--- app/dashboards/urls.py
+++ app/dashboards/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from dashboards.views import AdminDashboardView, HospitalDashboardView
+from dashboards.views import (
+    AdminDashboardView,
+    HospitalDashboardView,
+    DoctorDashboardView,
+)
 
 urlpatterns = [
     path("admin-stats/", AdminDashboardView.as_view(), name="admin-dashboard-stats"),
     path("hospital-stats/", HospitalDashboardView.as_view(), name="hospital-dashboard"),
+    path("doctor-stats/", DoctorDashboardView.as_view(), name="doctor-dashboard"),
 ]
